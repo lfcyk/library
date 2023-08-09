b088a955df01b27bc76228cf6a75c9c4cb95072f
Willa Waliyadin <59253075+lfcyk@users.noreply.github.com>
2023-08-10 00:20:34 +0700

    add comments

--- script.js
+++ script.js
@@ -24,44 +24,52 @@ addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false)
 addBookToLibrary("Dune", "Frank Herbert", 896, true)
 renderCards();
 
-
+// render cards
 function renderCards() {
 	myLibrary.map((book) => {
 		if(!book.isDeleted) {
+			// create element card
 			let card = document.createElement('div');
 			card.classList.add("book");
 			card.setAttribute('id', "book-" + myLibrary.indexOf(book));
-		
+
+			// create element delete button
 			let deleteButton = document.createElement('button');
 			deleteButton.classList.add("deleteButton");
 			deleteButton.setAttribute('data-book', myLibrary.indexOf(book))
 			deleteButton.textContent = "✖";
-		
+			
+			// create element title
 			let title = document.createElement('div');
 			title.classList.add("title");
 			title.textContent = book.title;
 		
+			// create element author
 			let author = document.createElement('div');
 			author.classList.add("author");
 			author.textContent = "by " + book.author;
-		
+			
+			// create element pages
 			let pages = document.createElement('div');
 			pages.classList.add("pages");
 			pages.textContent = book.pages + " pages";
 		
+			// create element read
 			let read = document.createElement('div');
 			read.classList.add("read");
 			read.textContent = book.read ? "read" : "not yet read";
 			
+			// append elements to card
 			card.appendChild(title);
 			card.appendChild(author);
 			card.appendChild(pages);
 			card.appendChild(read);
 			card.appendChild(deleteButton);
 		
-			
+			// append card to container
 			container.appendChild(card)
 
+			// when delete button is clicked
 			deleteButton.addEventListener("click", () => {
 				let deletedBookIndex = deleteButton.getAttribute("data-book");
 				myLibrary[deletedBookIndex].isDeleted = true;
@@ -74,6 +82,7 @@ function renderCards() {
 	})
 }
 
+//delete cards to rerender
 function deleteAllCards() {
 	let cards = document.querySelectorAll(".book");
 	cards.forEach((card) => {
@@ -82,23 +91,29 @@ function deleteAllCards() {
 }
 
 
+// show modal
 addBookButton.addEventListener("click",() => {   
     addBookModal.classList.remove("hidden");
     modalBackdrop.classList.remove("hidden");
 })
 
 
+// hide modal when backdrop clicked
 modalBackdrop.addEventListener("click", () => {
 	addBookModal.classList.add("hidden");
     modalBackdrop.classList.add("hidden");
 })
 
+// when user press submit button
 addBookSubmitButton.addEventListener("click", (event) => {
+
+	// get values from forms
 	let title = document.getElementById("title").value;
 	let author = document.getElementById("author").value;
 	let pages = document.getElementById("pages").value;
 	let read = document.getElementById("read").checked;
 	
+	// refuse to submit when title is empty
 	if(!title) {
 		console.log("no title");
 		event.preventDefault();
@@ -107,13 +122,17 @@ addBookSubmitButton.addEventListener("click", (event) => {
 
 	addBookToLibrary(title, author, pages, read);
 
+	//hide modal
 	addBookModal.classList.add("hidden");
     modalBackdrop.classList.add("hidden");
+
+	// reset forms values
 	document.getElementById("title").value = "";
 	document.getElementById("author").value = "";
 	document.getElementById("pages").value = "";
 	document.getElementById("read").checked = false;
 
+	// delete and rerender all the cards
 	deleteAllCards();
 	renderCards();
 
